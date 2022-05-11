1119189fb4f33941b2ee3812103fd3f4dda5c185
Оксана Широкова <son134@yandex.ru>
2022-05-11 20:22:56 +0300

    sprint7 final2

--- homework.py
+++ homework.py
@@ -1,18 +1,23 @@
-import logging.config
+import logging
 import os
 import requests
-import telebot
 import time
-import traceback
 
 from dotenv import load_dotenv
 from exceptions import HomeworkException
-from logging import Handler, LogRecord
 from http import HTTPStatus
 from telegram import Bot, error
 
 
 load_dotenv()
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(message)s',
+    filename='main.log',
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -29,66 +34,6 @@ HOMEWORK_VERDICTS = {
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
-
-
-# кастомный хендлер
-class TelegramBotHandler(Handler):
-    """Отправляет сообщение об ошибке в телеграм."""
-
-    def __init__(self, token: str, chat_id: str):
-        """Задает доступы к чатам."""
-        super().__init__()
-        self.token = token
-        self.chat_id = chat_id
-
-    def emit(self, record: LogRecord):
-        """Отправляет сообщение в телеграм."""
-        bot = telebot.TeleBot(self.token)
-        bot.send_message(self.chat_id, self.format(record))
-
-    def __str__(self) -> str:
-        """Описание класса."""
-        return 'Handler for sending logs to telegram'
-
-
-LOGGING_CONFIG = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'default_formatter': {
-            'format': '%(asctime)s %(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file_handler': {
-            'class': 'logging.FileHandler',
-            'formatter': 'default_formatter',
-            'filename': 'main.log'
-        },
-        'telegram_handler': {
-            'class': '__main__.TelegramBotHandler',
-            'token': TELEGRAM_TOKEN,
-            'chat_id': TELEGRAM_CHAT_ID,
-            'formatter': 'default_formatter'
-        },
-    },
-    'loggers': {
-        'my_logger': {
-            'handlers': ['file_handler'],
-            'level': 'INFO',
-            'propagate': True
-        },
-        'my_telegram_logger': {
-            'handlers': ['file_handler', 'telegram_handler'],
-            'level': 'ERROR',
-            'propagate': True
-        }
-    }
-}
-
-logging.config.dictConfig(LOGGING_CONFIG)
-logger1 = logging.getLogger('my_logger')
-logger2 = logging.getLogger('my_telegram_logger')
 
 
 # кастомный декоратор
@@ -109,18 +54,29 @@ def sleep_error(timeout, retry=3):
     return time_sleep_error
 
 
+def sending_error_to_telegram(message):
+    """Отправляет сообщение об ошибке в телеграм."""
+    previous_error = ""
+    while True:
+        new_error = message
+        if new_error != previous_error:
+            Bot(TELEGRAM_TOKEN).send_message(TELEGRAM_CHAT_ID, message)
+            previous_error = new_error
+
+
 def send_message(bot, message):
     """Отправляет сообщение в Telegram чат."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
-        logger1.info('The message has been sent')
+        logger.info('The message has been sent')
     except error.TelegramError:
-        logger2.error('TELEGRAM_CHAT_ID is not available')
+        message_telegram_error = 'TELEGRAM_CHAT_ID is not available'
+        logger.error(message_telegram_error)
+        sending_error_to_telegram(message_telegram_error)
     except Exception:
-        logger2.error(
-            'Error sending the message',
-            traceback.format_exc()
-        )
+        message_exception_error = 'Error sending the message'
+        logger.error(message_exception_error)
+        sending_error_to_telegram(message_exception_error)
 
 
 def get_api_answer(current_timestamp):
@@ -133,109 +89,120 @@ def get_api_answer(current_timestamp):
             headers=HEADERS,
             params=params
         )
-        logger1.info('API request sent')
+        logger.info('API request sent')
         if homework_statuses.status_code != HTTPStatus.OK:
             text_error = (
                 f'The API returned the code {homework_statuses.status_code}'
             )
-            logger2.error(text_error)
-            raise HomeworkException(text_error)
+            logger.error(text_error)
+            sending_error_to_telegram(text_error)
     except ConnectionError:
-        logger2.error('The endpoint is unavailable')
+        message_connection_error = 'The endpoint is unavailable'
+        logger.error(message_connection_error)
+        sending_error_to_telegram(message_connection_error)
     except Exception:
-        logger2.error(
-            'Failure when requesting an endpoint',
-            traceback.format_exc()
-        )
+        message_exception_error = 'Failure when requesting an endpoint'
+        logger.error(message_exception_error)
+        sending_error_to_telegram(message_exception_error)
+        raise HomeworkException(message_exception_error)
     response = homework_statuses.json()
-    logger1.info('API response received')
+    logger.info('API response received')
     return response
 
 
 def check_response(response):
     """Проверка ответа API на корректность и выдача списка домашек."""
-    logger1.info('Checking the correctness of the API response')
+    logger.info('Checking the correctness of the API response')
     if response.get('homeworks') is None:
         error_text = 'Dictionary key not found'
-        logger2.error(error_text)
+        logger.error(error_text)
+        sending_error_to_telegram(error_text)
         raise HomeworkException(error_text)
     if isinstance(response['homeworks'], list) is False:
         error_message = 'The API response is not a list'
-        logger2.error(error_message)
+        logger.error(error_message)
+        sending_error_to_telegram(error_message)
         raise HomeworkException(error_message)
-    logger1.info('The API response is correct. Received a list of homework')
+    logger.info('The API response is correct. Received a list of homework')
     return response['homeworks']
 
 
 def parse_status(homework):
     """Извлекает статус домашки и возвращает строчку для отправки."""
-    logger1.info('Determining the status of homework')
+    logger.info('Determining the status of homework')
     try:
         homework_name = homework['homework_name']
         homework_status = homework['status']
     except KeyError:
         message_error = 'Key not found'
-        logger2.error(message_error)
+        logger.error(message_error)
+        sending_error_to_telegram(message_error)
         raise HomeworkException(message_error)
     else:
         if homework_status in HOMEWORK_VERDICTS:
             verdict = HOMEWORK_VERDICTS[homework_status]
-            logger1.info('Received the text for the message')
+            logger.info('Received the text for the message')
             return (
                 f'Изменился статус проверки работы "{homework_name}". '
                 f'{verdict}'
             )
         message_text = 'Unknown homework status'
-        logger2.error(message_text)
+        logger.error(message_text)
+        sending_error_to_telegram(message_text)
         raise HomeworkException(message_text)
 
 
 def check_tokens():
     """Проверяет доступность переменных окружения."""
-    logger1.info('Checking the availability of environment variables')
+    logger.info('Checking the availability of environment variables')
     if PRACTICUM_TOKEN is None:
-        logger2.critical(
+        text_error = (
             'Required environment variable is missing: PRACTICUM_TOKEN'
         )
+        logger.critical(text_error)
+        sending_error_to_telegram(text_error)
         return False
     if TELEGRAM_TOKEN is None:
-        logger2.critical(
+        logger.critical(
             'Required environment variable is missing: TELEGRAM_TOKEN'
         )
         return False
     if TELEGRAM_CHAT_ID is None:
-        logger2.critical(
+        logger.critical(
             'Required environment variable is missing: TELEGRAM_CHAT_ID'
         )
         return False
-    logger1.info('Environment variables are available')
+    logger.info('Environment variables are available')
     return True
 
 
 @sleep_error(TIME_SLEEP)
 def main():
     """Основная логика работы бота."""
-    logger1.info('Start Bot')
+    logger.info('Start Bot')
     bot = Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    current_timestamp = int(time.time()) - RETRY_TIME
     try:
         response = get_api_answer(current_timestamp)
         homeworks = check_response(response)
         if len(homeworks) > 0:
             send_message(bot, parse_status(homeworks[0]))
         else:
-            logger2.error('The homework list is empty')
+            text_error = 'The homework list is empty'
+            logger.error(text_error)
+            sending_error_to_telegram(text_error)
         current_timestamp = response['current_date']
+        time.sleep(RETRY_TIME)
     except IndexError:
         error_text = 'List index out of range'
-        logger2.error(error_text)
+        logger.error(error_text)
+        sending_error_to_telegram(error_text)
         raise HomeworkException(error_text)
     except Exception as error:
         message = f'Program malfunction: {error}'
-        logger2.error(message, traceback.format_exc())
+        logger.error(message)
+        sending_error_to_telegram(message)
         raise HomeworkException(message)
-    else:
-        time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
